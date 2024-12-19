dc3eceaa408661bae62076e2b1f49817ffc90c68
Rengkat <successinmotion11@gmail.com>
2024-12-19 03:24:10 +0100

    register mutation

--- src/Redux/Api/AuthApiSplice.js
+++ src/Redux/Api/AuthApiSplice.js
@@ -1,11 +1,19 @@
 import { createApi } from "@reduxjs/toolkit/query/react";
 import { baseQueryUrl } from "./BaseApiUrl";
+const AUTH_URL = "auth";
 export const authApiSlice = createApi({
   reducerPath: "authApi",
   baseQuery: baseQueryUrl,
   tagTypes: [],
   endpoints: (build) => ({
     //register mutation
+    register: build.mutation({
+      query: (data) => ({
+        url: `${AUTH_URL}/register`,
+        method: "POST",
+        body: data,
+      }),
+    }),
     // verify email mutation
     //login mutation
     //logout
--- src/Redux/Store.js
+++ src/Redux/Store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import AppSlice from "./Services/AppSlice";
+import { authApiSlice } from "./Api/AuthApiSplice";
 
 export const store = configureStore({
   reducer: {
     AppSlice: AppSlice,
+    [authApiSlice.reducerPath]: authApiSlice.reducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(authApiSlice.middleware),
 });
