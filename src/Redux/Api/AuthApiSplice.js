c10ab058509471a9f098ef087f49f6dad367be2f
Rengkat <successinmotion11@gmail.com>
2024-12-19 03:30:34 +0100

    logout mutation

--- src/Redux/Api/AuthApiSplice.js
+++ src/Redux/Api/AuthApiSplice.js
@@ -31,5 +31,12 @@ export const authApiSlice = createApi({
       }),
     }),
     //logout
+    logout: build.mutation({
+      query: (data) => ({
+        url: `${AUTH_URL}/logout`,
+        method: "POST",
+        body: {},
+      }),
+    }),
   }),
 });
