0a8bde0546d0b2738a5372cbbe6420bd13b17248
Rengkat <successinmotion11@gmail.com>
2024-12-26 08:16:27 +0100

    news, event, blogs api

---
+++ src/Redux/Api/BlogApiSlice.js
@@ -0,0 +1,42 @@
+import { createApi } from "@reduxjs/toolkit/query/react";
+import { baseQueryUrl } from "./BaseApiUrl";
+const EVENTS_URL = "events";
+const NEWS_BLOG_URL = "news-and-blogs";
+
+export const blogApiSlice = createApi({
+  reducerPath: "blogsApi",
+  baseQuery: baseQueryUrl,
+  tagTypes: ["Blog"],
+  endpoints: (build) => ({
+    //get all events
+    getAllEvents: build.query({
+      query: () => ({
+        url: `${EVENTS_URL}`,
+        method: "GET",
+      }),
+    }),
+    //get all news, blogs and events
+    getAllBlogsAndNewsEvents: build.query({
+      query: () => ({
+        url: `${NEWS_BLOG_URL}`,
+        method: "GET",
+      }),
+    }),
+    //get all news
+    getAllNews: build.query({
+      query: () => ({
+        url: `${NEWS_BLOG_URL}/news`,
+        method: "GET",
+      }),
+    }),
+    //get all Blogs
+    getAllBlogs: build.query({
+      query: () => ({
+        url: `${NEWS_BLOG_URL}/blogs`,
+        method: "GET",
+      }),
+    }),
+  }),
+});
+
+export const { useGetAllSetEventsQuery, useGetSetEventByIdQuery } = blogApiSlice;
